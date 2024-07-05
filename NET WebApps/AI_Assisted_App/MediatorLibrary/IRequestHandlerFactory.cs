16d7212484f8989f885c85384c944ee8735cb978
Ryan Lintag <77764315+ryanlintag-who@users.noreply.github.com>
2024-07-06 00:48:01 +0800

    Refactoring and Added Unit Tests

---
+++ NET WebApps/AI_Assisted_App/MediatorLibrary/IRequestHandlerFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MediatorLibrary
+{
+    public interface IRequestHandlerFactory
+    {
+        object CreateHandler(Type handlerType);
+        IRequestHandler<TRequest, TResponse> CreateHandler<TRequest, TResponse>(Type handlerType)
+            where TRequest : IRequest<TResponse>
+            where TResponse : class;
+    }
+}
